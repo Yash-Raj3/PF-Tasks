17eb1f8ca380911449e7390062aa84b80e5deb46
Yash Raj <131522276+Yash-Raj3@users.noreply.github.com>
2025-10-13 10:43:11 +0500

    Add files via upload

---
+++ Array.c
@@ -0,0 +1,81 @@
+
+#include<stdio.h>
+int main(){
+    /*There are twpo types of Datatype 1.Builtin/primitive like float,int,char,bool.
+    2.Derived like array,function,Pointers
+	1D Array Stores Values in only One row*/
+//int studentAges[]={1,2,3,4,5};
+//printf("%d\n",sizeof(studentAges)/sizeof(studentAges[0]));
+//for (int i=0;i<(sizeof(studentAges)/sizeof(studentAges[0]));i++){
+//	printf(" You have %d\t",studentAges[i]);
+//}
+//
+//int countl =0;
+//char charworld[5]="Hello";
+//printf("%s\n",charworld);
+//for (int i=0;i<sizeof(charworld);i++){
+//	if(charworld == 'l'||charworld=='L' ){
+//		countl++;
+//	}
+//}
+
+
+
+
+//char repeat[5]={'H','E','L','L','O'};
+//printf("%c",repeat);
+
+//int userAges[10],lowest=0;
+//printf("Enter 10  students Ages : ");
+//for (int i=0;i<10;i++){
+//    scanf("%d",&userAges[i]);
+//}
+//int rev[10],i;
+//for ( i=0;i<10;i++){
+//      rev[i]=userAges[9-i];
+//}
+
+//for ( i=9;i>=0;i--){
+//printf("Reverse is %d\t",userAges[i]);
+//}
+//char A[100];
+//printf("Enter Your Name :");
+//scanf("%[^a-z@1-9]",A);
+//printf("Your Name is %s",A);
+//int Idcheck,F;
+//int id[12],i,f=0;
+//printf("Enter Your Ids:");
+//for(i=0;i<12;i++){
+//	scanf("%[0-9]",&id[i]);
+//}
+//printf("Enter Id to  Find:");
+//scanf("%d",&Idcheck);
+//for (i=0;i<12;i++){
+//	if(id[i]==idf){
+//		f=1;
+//		F=i;
+//	}
+//}
+//if(f){
+//	Printf("Your Id is Found At the Number %d",F);
+//}else {
+//	printf("Your Id is not in the record!");
+//}
+//
+//return  0;
+//}
+//int marks[10];
+//int i;
+//printf("Enter 10 students Marks:");
+//for(i=0;i<10;i++){
+//	scanf("%[0-9]",marks[i]);
+//}
+//int sum =0;
+//for(i=0;i<10;i++){
+//	sum += marks[i];
+//}
+//printf("Average of 10 students is %d",sum/10);
+//return0;
+//}
+
+
