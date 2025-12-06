b51149219c31ddad942348644b071bbda7010c37
Yash-Raj3 <yrajbhagdev@gmail.com>
2025-12-06 19:46:29 +0500

    Remove

--- yash/Programmes/Lab Task 3/labtask3-6.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main(){
-    printf("Hello Fastians!\n");
-    printf("Today's topic is introduction to Github");
-    return 0;
-}
--- yash/m.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    int a=3,b=4;
-    a = a^b;
-    b =a^b;
-    a = a^b;
-    printf("%d %d ",a,b);
-    return 0;
-}
